bebf51e44a9db9830fb1dcea211c59bd36405503
Joachim Toft <joachimt@spotify.com>
2020-04-24 11:52:26 +0200

    Updates

---
+++ precache-manifest.38fd9097a455719ea5b61ef6b65fc30f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5771b75f22a18142a9e96f2a1160f202",
+    "url": "/robot-tic-tac-toe/7362b3c3f320ddb0e423.worker.js"
+  },
+  {
+    "revision": "40fb7686c040360377dcb3380ea9adfb",
+    "url": "/robot-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "affa20addca5fe7428cc",
+    "url": "/robot-tic-tac-toe/static/js/2.e096189b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/robot-tic-tac-toe/static/js/2.e096189b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01ac599d9ba45967e6c9",
+    "url": "/robot-tic-tac-toe/static/js/main.fa189fc0.chunk.js"
+  },
+  {
+    "revision": "59358cc9cc07646af79d",
+    "url": "/robot-tic-tac-toe/static/js/runtime-main.fec52621.js"
+  }
+]);
